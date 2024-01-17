9f90b52ea29d2f68ebe96832df1e0c1f625d70cb
Andreas Nygård <anny2301@student.miun.se>
2024-01-17 17:02:39 +0100

    Added responsive menu with functionality

---
+++ src/js/close.js
@@ -0,0 +1,6 @@
+let closeEl = document.getElementById("close");
+let navMobileCloseEl = document.getElementById("nav-mobile");
+
+closeEl.addEventListener("click", function(){
+    navMobileCloseEl.style.display = "none";
+})
---
+++ src/js/hamburger.js
@@ -0,0 +1,6 @@
+let hamburgerEl = document.getElementById("hamburger");
+let navMobileEl = document.getElementById("nav-mobile");
+
+hamburgerEl.addEventListener("click", function(){
+    navMobileEl.style.display = "block";
+})
