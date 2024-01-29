7c431dae42fed1a076af47e76258f162e910378b
Andreas Nygård <anny2301@student.miun.se>
2024-01-29 20:11:38 +0100

    Installed "Prettier"

--- src/js/schedule.js
+++ src/js/schedule.js
@@ -10,90 +10,91 @@ let sortDirection = true;
 window.onload = init;
 
 async function init() {
-    try {
-        const response = await fetch(url);
-        let scheduleList = await response.json();
+  try {
+    const response = await fetch(url);
+    let scheduleList = await response.json();
 
-        //Eventlyssnare för sortering 
-        cProgEl.addEventListener("click", function(){
-            sortDirection = !sortDirection;
-            sortArrayByProgression(scheduleList);
-            displaySchedule(scheduleList);
-        });
+    //Eventlyssnare för sortering
+    cProgEl.addEventListener("click", function () {
+      sortDirection = !sortDirection;
+      sortArrayByProgression(scheduleList);
+      displaySchedule(scheduleList);
+    });
 
-        cCodeEl.addEventListener("click", function(){
-            sortDirection = !sortDirection;
-            sortArrayByCode(scheduleList);
-            displaySchedule(scheduleList);
-        });
+    cCodeEl.addEventListener("click", function () {
+      sortDirection = !sortDirection;
+      sortArrayByCode(scheduleList);
+      displaySchedule(scheduleList);
+    });
 
-        cNameEl.addEventListener("click", function(){
-            sortDirection = !sortDirection;
-            sortArrayByName(scheduleList);
-            displaySchedule(scheduleList);
-        })
+    cNameEl.addEventListener("click", function () {
+      sortDirection = !sortDirection;
+      sortArrayByName(scheduleList);
+      displaySchedule(scheduleList);
+    });
 
-        //Filtera vid sökning
-        searchFieldEl.addEventListener("input", function(){
-            let searchInput = searchFieldEl.value;
+    //Filtera vid sökning
+    searchFieldEl.addEventListener("input", function () {
+      let searchInput = searchFieldEl.value;
 
-            displaySchedule(filterSchedule(scheduleList, searchInput));
-        })
+      displaySchedule(filterSchedule(scheduleList, searchInput));
+    });
 
-        sortArrayByName(scheduleList);
-        displaySchedule(scheduleList);
-
-    } catch {
-        document.getElementById("error").innerHTML = "<p>Något gick fel...</p>";
-    }
+    sortArrayByName(scheduleList);
+    displaySchedule(scheduleList);
+  } catch {
+    document.getElementById("error").innerHTML = "<p>Något gick fel...</p>";
+  }
 }
 
-function filterSchedule(schedule, searchInput){
-    return schedule.filter(schedule => {
-        let name = schedule.coursename.toLowerCase();
-        let code = schedule.code.toLowerCase();
+function filterSchedule(schedule, searchInput) {
+  return schedule.filter((schedule) => {
+    let name = schedule.coursename.toLowerCase();
+    let code = schedule.code.toLowerCase();
 
-        return name.includes(searchInput.toLowerCase()) || code.includes(searchInput.toLowerCase());
-    });
+    return (
+      name.includes(searchInput.toLowerCase()) ||
+      code.includes(searchInput.toLowerCase())
+    );
+  });
 }
 
 function displaySchedule(schedule) {
-    const scheduleListEl = document.getElementById("schedule-list");
-    scheduleListEl.innerHTML = "";
+  const scheduleListEl = document.getElementById("schedule-list");
+  scheduleListEl.innerHTML = "";
 
-    schedule.forEach((schedule) => {
-        scheduleListEl.innerHTML += `
+  schedule.forEach((schedule) => {
+    scheduleListEl.innerHTML += `
         <tr>
             <td>${schedule.code}</td>
             <td>${schedule.coursename}</td>
             <td>${schedule.progression}</td>
         </tr>
         `;
-    });
+  });
 }
 
 //Sortera lista efter namn
-function sortArrayByName(schedule){
-    if(sortDirection){
-        schedule.sort((a, b) => (a.coursename > b.coursename) ? 1 : -1);
-    }else{
-        schedule.sort((a, b) => (a.coursename > b.coursename) ? 1 : -1).reverse();
-    }
+function sortArrayByName(schedule) {
+  if (sortDirection) {
+    schedule.sort((a, b) => (a.coursename > b.coursename ? 1 : -1));
+  } else {
+    schedule.sort((a, b) => (a.coursename > b.coursename ? 1 : -1)).reverse();
+  }
 }
 
 function sortArrayByCode(schedule) {
-    if(sortDirection){
-        schedule.sort((a, b) => (a.code > b.code) ? 1 : -1);
-    }else {
-        schedule.sort((a, b) => (a.code > b.code) ? 1 : -1).reverse();
-    }
+  if (sortDirection) {
+    schedule.sort((a, b) => (a.code > b.code ? 1 : -1));
+  } else {
+    schedule.sort((a, b) => (a.code > b.code ? 1 : -1)).reverse();
+  }
 }
 
 function sortArrayByProgression(schedule) {
-    if(sortDirection) {
-        schedule.sort((a, b) => (a.progression > b.progression) ? 1 : -1);
-    }else {
-        schedule.sort((a, b) => (a.progression > b.progression) ? 1 : -1).reverse();
-    }
+  if (sortDirection) {
+    schedule.sort((a, b) => (a.progression > b.progression ? 1 : -1));
+  } else {
+    schedule.sort((a, b) => (a.progression > b.progression ? 1 : -1)).reverse();
+  }
 }
-
